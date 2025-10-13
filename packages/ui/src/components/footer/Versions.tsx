40e43fa58de3792cd89efa7e74cd5d78131343f9
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2025-10-12 19:16:22 -0600

    fix(ui): temporarily disable update checking entirely

--- packages/ui/src/components/footer/Versions.tsx
+++ packages/ui/src/components/footer/Versions.tsx
@@ -1,8 +1,4 @@
-import clsx from 'clsx';
-import {useEffect} from 'preact/hooks';
-import {useState} from 'react';
 import {useApplication} from '../../contexts';
-import {compareVersions} from '../../utils';
 import styles from './Versions.module.scss';
 
 export function Versions() {
@@ -11,35 +7,9 @@ export function Versions() {
     core: '0.0.0',
     ...(project.versions ?? {}),
   };
-  const [newVersion, setNewVersion] = useState(versions.core);
-  const isOld = compareVersions(versions.core, newVersion) < 0;
-
-  useEffect(() => {
-    const abort = new AbortController();
-    fetch('https://registry.npmjs.org/@canvas-commons/core/latest', {
-      signal: abort.signal,
-    })
-      .then(response => response.json())
-      .then(response => setNewVersion(response.version))
-      .catch(error => {
-        console.error(error);
-        setNewVersion(versions.core);
-      });
-    return () => abort.abort();
-  }, []);
 
   return (
     <div className={styles.root}>
-      {isOld && (
-        <a
-          href="https://github.com/canvas-commons/canvas-commons/releases"
-          target="_blank"
-          title="See what's new"
-          className={clsx(styles.link, styles.main)}
-        >
-          UPDATE AVAILABLE
-        </a>
-      )}
       <div
         title="Copy version information"
         className={styles.link}
